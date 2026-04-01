4d40b6798bb1dd0bf7013569e953b3232efd4159
wwolos <wwolos.mail@gmail.com>
2026-04-01 22:44:28 +0200

    Initial

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,         -- Enable this!
+      styles = {
+        sidebars = "transparent", -- Sidebars (neo-tree, etc.)
+        floats = "transparent",   -- Floating windows
+      },
+    },
+  },
+}
