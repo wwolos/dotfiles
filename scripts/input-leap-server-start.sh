bd387c715234d0a292358f4009e9a9949c8870ae
wwolos <wwolos.mail@gmail.com>
2026-05-17 12:22:39 +0200

    a

--- scripts/input-leap-server-start.sh
+++ scripts/input-leap-server-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo input-leaps --config ~/dotfiles/input-leap/input-leap.conf --restart --log ./input-leaps.log --disable-crypto -f
+input-leaps --config ~/dotfiles/input-leap/input-leap.conf --restart --log ./input-leaps.log --disable-crypto -f
