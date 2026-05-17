e54bb9e4f640589455bdd0611a5aba616f3f272d
wwolos <wwolos.mail@gmail.com>
2026-05-17 12:25:21 +0200

    a

--- scripts/input-leap-client-start.sh
+++ scripts/input-leap-client-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-input-leapc --name LAPTOP --restart --log input-leapc.log --disable-crypto -f
+input-leapc --name LAPTOP --restart --log input-leapc.log --disable-crypto -f 192.168.1.14
